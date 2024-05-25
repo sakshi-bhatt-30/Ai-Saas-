53898b3626cd418319d87eb3a856edfd8a3eb3f6
sakshi-bhatt-30 <129189519+sakshi-bhatt-30@users.noreply.github.com>
2024-05-26 03:17:19 +0530

    Added components and routes

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+export default clerkMiddleware({
+    // publicRoutes:["/"]
+});
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
